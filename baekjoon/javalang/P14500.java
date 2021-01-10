d47f5e8c3bd6154a8d34a51cd0f728c94e434451
HyunjoonCho <jhj09016@gmail.com>
2021-01-10 21:56:38 +0900

    test commit

---
+++ baekjoon/javalang/P14500.java
@@ -0,0 +1,17 @@
+package javalang;
+
+class P14500 {
+    public static void main(String[] args) {
+        
+    }
+}
+
+/*
+greedy works? i.e. max of 4 == max of 3 + n
+No 
+4 4 4 1
+1 1 1 1  
+3 4 3 4
+
+brute force? 2 + 1 + 8 + 8 + 4 = 23 cases
+ */
