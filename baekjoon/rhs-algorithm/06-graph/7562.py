e7a1b59e9ea9dc4ebaa6376a0f1698d2d5e94104
HyunjoonCho <jhj09016@gmail.com>
2021-08-21 17:45:39 +0900

    [#4] Graph - now it works..

--- baekjoon/rhs-algorithm/06-graph/7562.py
+++ baekjoon/rhs-algorithm/06-graph/7562.py
@@ -1,26 +1,27 @@
 from sys import stdin
+from collections import deque
 
 T = int(stdin.readline())
-
+deltas = [(-2, -1), (-2, 1), (-1, 2), (1, 2), (2, 1), (2, -1), (1, -2), (-1, -2)]
 
 for _ in range(T):
     l = int(stdin.readline())
     startR, startC = map(int, stdin.readline().split())
     endR, endC = map(int, stdin.readline().split())
 
-    if startR == endR and startC == endC:
-        print(0)
-    else:
-        distMap = [[-1] * l for _ in range(l)]
-        queue = [(startR, startC, 0)]
-        while queue:
-            r, c, dist = queue.pop(0)
-            if distMap[r][c] == -1:
-                distMap[r][c] = dist
-                if r == endR and c == endC:
-                    print(dist)
-                    break
-                else:
-                    for delR, delC in [(-2, -1), (-2, 1), (2, -1), (2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2)]:
-                        if 0 <= r + delR < l and 0 <= c + delC < l:
-                            queue.append((r + delR, c + delC, dist + 1))
+    distMap = [[0] * l for _ in range(l)]
+    queue = deque()
+    queue.append((startR, startC))
+    distMap[startR][startC] = 1
+    while queue:
+        r, c = queue.popleft()
+        if r == endR and c == endC:
+            print(distMap[r][c] - 1)
+            break
+        else:
+            for delR, delC in deltas:
+                nextR = r + delR
+                nextC = c + delC
+                if 0 <= nextR < l and 0 <= nextC < l and not distMap[nextR][nextC]:
+                    distMap[nextR][nextC] = distMap[r][c] + 1
+                    queue.append((nextR, nextC))
