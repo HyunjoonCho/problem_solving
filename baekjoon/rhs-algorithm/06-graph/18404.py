989eef9fd151ec57bbf873122889400883e23508
HyunjoonCho <jhj09016@gmail.com>
2021-08-21 22:39:47 +0900

    [#4] Graph - one bfs is good enough

--- baekjoon/rhs-algorithm/06-graph/18404.py
+++ baekjoon/rhs-algorithm/06-graph/18404.py
@@ -3,33 +3,28 @@ from collections import deque
 
 N, M = map(int, sys.stdin.readline().split())
 
-knightC, knightR = map(int, sys.stdin.readline().split())
+knightC, knightR = map(lambda x : int(x) - 1, sys.stdin.readline().split())
 
 dr = [-2, -2, -1, 1, 2, 2, 1, -1]
 dc = [-1, 1, 2, 2, 1, -1, -2, -2]
 
-distMap = [[0] * (N + 1) for _ in range(N + 1)]
+distMap = [[0] * N for _ in range(N)]
 distMap[knightR][knightC] = 1
-queue = deque()
-for _ in range(M):
-    targetC, targetR = map(int, sys.stdin.readline().split())
+result = []
 
-    if distMap[targetR][targetC]:
-        sys.stdout.write(str(distMap[targetR][targetC] - 1))
-    else:
-        queue.append((knightR, knightC))
-        while queue:
-            r, c = queue.popleft()
-            if r == targetR and c == targetC:
-                sys.stdout.write(str(distMap[r][c] - 1))
-                break
-            else:
-                for i in range(8):
-                    nextR = r + dr[i]
-                    nextC = c + dc[i]
-                    if 1 <= nextR <= N and 1 <= nextC <= N:
-                        if not distMap[nextR][nextC]:
-                            distMap[nextR][nextC] = distMap[r][c] + 1
-                        queue.append((nextR, nextC))
-        queue.clear()
-    sys.stdout.write(' ')
+queue = deque()
+queue.append((knightR, knightC))
+while queue:
+    r, c = queue.popleft()
+    for i in range(8):
+        nextR = r + dr[i]
+        nextC = c + dc[i]
+        if 0 <= nextR < N and 0 <= nextC < N and not distMap[nextR][nextC]:
+            distMap[nextR][nextC] = distMap[r][c] + 1
+            queue.append((nextR, nextC))
+
+for _ in range(M):
+    targetC, targetR = map(lambda x : int(x) - 1, sys.stdin.readline().split())
+    result.append(str(distMap[targetR][targetC] - 1))
+
+print(' '.join(result))
