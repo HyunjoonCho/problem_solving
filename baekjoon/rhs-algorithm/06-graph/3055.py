85da797e32c3cdc88c434a9b4fe23e3a263715cf
HyunjoonCho <jhj09016@gmail.com>
2021-08-27 23:27:48 +0900

    [#4] Graph - check whether a point is visited

--- baekjoon/rhs-algorithm/06-graph/3055.py
+++ baekjoon/rhs-algorithm/06-graph/3055.py
@@ -1,8 +1,9 @@
 from sys import stdin
+from collections import deque
 
 R, C = map(int, stdin.readline().split())
 forestMap = []
-waterLocs = []
+waterLocs = deque()
 for i in range(R):
     forestMap.append(list(stdin.readline().strip()))
     for j in range(C):
@@ -16,12 +17,17 @@ for i in range(R):
             forestMap[i][j] = 'X'
 
 deltas = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-currentLocs = [start]
+
+currentLocs = deque()
+currentLocs.append(start)
 time = 0
 isReached = False
+visited = [[False] * C for _ in range(R)]
+visited[start[0]][start[1]] = True
 while currentLocs:
-    nextWaterLocs = []
-    for r, c in waterLocs:
+    waterLen = len(waterLocs)
+    for _ in range(waterLen):
+        r, c = waterLocs.popleft()
         for dr, dc in deltas:
             nextR = r + dr
             nextC = c + dc
@@ -29,31 +35,29 @@ while currentLocs:
                 block = forestMap[nextR][nextC]
                 if block == '.':
                     forestMap[nextR][nextC] = '*'
-                    if (nextR, nextC) not in nextWaterLocs:
-                        nextWaterLocs.append((nextR, nextC))
+                    waterLocs.append((nextR, nextC))
     
     time += 1
-    nextLocs = []
-    for r, c in currentLocs:
+    locsLen = len(currentLocs)
+    for _ in range(locsLen):
+        r, c = currentLocs.popleft()
         for dr, dc in deltas:
             nextR = r + dr
             nextC = c + dc
             if (nextR, nextC) == end:
                 isReached = True
                 break
-            if 0 <= nextR < R and 0 <= nextC < C:
+            if 0 <= nextR < R and 0 <= nextC < C and not visited[nextR][nextC]:
+                visited[nextR][nextC] = True
                 block = forestMap[nextR][nextC]
                 if block == '.':
-                    nextLocs.append((nextR, nextC))
+                    currentLocs.append((nextR, nextC))
         if isReached:
             break
     if isReached:
         break
-    currentLocs = nextLocs
-    waterLocs = nextWaterLocs
 
 if isReached:
     print(time)
 else:
     print('KAKTUS')
-            
